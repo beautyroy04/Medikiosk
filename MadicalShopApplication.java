8be7736b316ca33f71c413a13cb6e4cd0d8fcd29
beautyroy04 <71842202+beautyroy04@users.noreply.github.com>
2020-09-26 21:05:19 +0530

    Add files via upload
    application code

---
+++ MadicalShopApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.madicalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadicalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadicalShopApplication.class, args);
+	}
+
+}
